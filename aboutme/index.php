d7214453f79169dbca9335d18d91d50c5775bf5c
matthes <matthesboettcher03@gmail.com>
2025-02-19 16:27:52 +0100

    Ueber Mich - Seite vorerst fertiggestellt

--- aboutme/index.php
+++ aboutme/index.php
@@ -5,24 +5,155 @@
     <?php include '../html/head.php'; ?>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu|Lora">
+    <link href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,100..900;1,100..900&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Albert+Sans:ital,wght@0,100..900;1,100..900&family=Archivo:ital,wght@0,100..900;1,100..900&family=Encode+Sans:wght@100..900&family=Liter&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&display=swap" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&display=swap" rel="stylesheet">
+
     <link rel="stylesheet" href="../Style/aboutme.css">
-    <link href="https://fonts.googleapis.com/css2?family=Edu+AU+VIC+WA+NT+Dots:wght@400..700&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Londrina+Sketch&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 
 <body class="gradient">
     <div>
         <?php include '../html/header.php'; ?>
 
-        <div class="container text-center mt-4">
-            <h3 class='work-sans'>
-                Hallo hallo hallo,<br>wie ich auf der Startseite erwähnt habe, dauert alles noch ein bisschen
-                länger. Ich möchte - wenn ich schon etwas hochlade - auch authentisch sein. Und das bin ich
-                nicht, wenn ich versuche alles so schnell es geht zu schreiben
-            </h3>
-        </div>
+        <div class="container text_cont rounded p-4 mt-3">
+            <div class="m-1">
+                <p class="h1 text-center title mt-2">Über mich</p>
+                
+                <div class="row align-items-center">
+                    <div class="col col-md">
+                        <p class="h2">Ein 
+                            <span class="fs-6">
+                                kleines
+                            </span> 
 
-        <?php include 'p_multimengen.php'; ?>
+                            Intro</p>
+                        <p class="fs-5">
+                            Es ist ironisch.
+                            Ich habe mir für diese Seite sehr viel Zeit gelassen. Um genau zu sein: um die 3
+                            Monate. Ich habe mir bestimmt um die 523.428 Gedanken gemacht, was ich hier überhaupt
+                            schreiben möchte. Habe meine ganzen Notizen voll mit Gedanken gefüllt. Und das alles nur
+                            dafür, dass ich das hier spontan schreibe. Keine Vorbereitung, kein nichts. Einfach 
+                            meine Gedanken fließen lassen.
+                            <small class="text-light">
+                            Auch wenn ich mich gerade für 25 Minuten mit <i>Schriftarten</i> auseinandergesetzt
+                            habe (ich mag es nicht). Dafür siehst du gerade eine hoffentlich angenehme Schrift!
+                            </small>
+                        </p>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="d-flex justify-content-center">
+                            <img src="../Assets/me.JPEG" class="w-75 img-thumbnail rounded" alt="ein nettes Bild von mir">
+                        </div>
+                        <div class="d-flex justify-content-center">
+                            <p class="figure-caption text-light text-center">
+                                Ein nettes Bild das dich von dem sich <i>färbenden</i> Hintergrundbild ablenkt
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <hr class="mt-4" style="border-width: 0.5vh; width: 90%; margin:auto;">
+                <div class="mt-5">
+                    <p class="h2">WIM</p>
+                    <p class="fs-5">Also, zum eigentlichen Thema: wer bin ich? Wer ist Matthes?</p>
+                    <p class="fs-5">Das zu beantworten könnte schwer werden. Ich bin auf jeden Fall 'vielseitig'</p>
+                    <p class="fs-5">Ich meine... versuch du dich doch mal zu beschreiben. Das ist gar nicht so leicht🙁</p>
+                    <p class="fs-5">
+                        Ich bin in manchen Bereichen ein unglaublicher 'nerd'. Liebe es Schach zu spielen
+                        und mich mit Technologie auseinanderzusetzen... Auch nach 1,5 Jahren Informatikstudium stehe
+                        ich regelmäßig vor dem Spiegel und frage mich wieso ein Stück Metall Licht und Ton und
+                        Elektrizität machen kann. ICH MEINE WIE????? Also ja; ich kann es einigermaßen erklären,
+                        aber selbst damit ist es in gewisser weise nichts als magisch
+                        <br>
+                        Naja, I digress - ich schweife ab.
+                        <br>
+                        Seitdem ich in Leipzig wohne, ist es für mich immer mehr zu einer art Aufgabe geworden, an
+                        mir selbst zu arbeiten und mich weiterzuentwickeln. Ich glaube ich habe mich noch nie so sehr
+                        in einer so 'kurzen' Zeit (1,5 Jahre) verändert. In etwas gutes? Etwas besseres?
+                        Das weiß ich nicht. Ich nehme mir trotz allem zu wenig Zeit mich zu grounden und auf alles
+                        klarzukommen. Etwas an das ich weiterarbeiten kann!
+                    </p>
+                </div>
+                <div> 
+                    <p class="fs-5">
+                        Jeder der diesen Text liest und mich länger kennt, wird vermutlich nicht an mich als Person
+                        denken, mit der man ab und zu was gemacht hat. Eher an ein gewisses "Wow, dieser Mensch ist so
+                        intelligent und hübsch und toll und hat so eine coole Website"
+                    </p>
+                    <p class="fs-5">
+                        Nur ein Spaß😁
+                    </p>
+                    <p class="fs-5">
+                        Ich weiß gar nicht wie ich das beschreiben soll. Aber du verstehst doch bestimmt was ich meine.
+                        Jede Person verhält sich im Internet anders. Sei es beim Bild machen, schreiben, Video oder Audio
+                        aufnehmen. Nie ist man so wirklich sich selbst. Immer denkt man eher an das, was die andere Person
+                        davon hält, anstatt zu 100% <i>authentisch</i> zu sein - aber das ist ja auch absolut nicht 
+                        schlimm. Das ist mir gerade nur so aufgefallen.
+                    </p> 
+                    <p class="fs-5">
+                        Und wow, mir fällt gerade auf dass dieser Text wirklich genau so strukturiert und durchdacht wie 
+                        die Briefe die ich damals mit einer Freundin ausgetauscht habe. Also überhaupt nicht strukturiert 
+                        geschweigedenn durchdacht. Das werde ich bestimmt mal überarbeiten. Aber für eine erste 'Skizze' 
+                        ist das doch schon mal ein Anfang😄
+                    </p>
+                    <p class="fs-5">
+                        und beeindruckend dass du das alles gelesen hast! 
+                    </p>
+                </div>
+                
+                <div class="mt-5">
+                <hr class="mt-4" style="border-width: 0.5vh; width: 90%; margin:auto;">
+                    <br>
+                    <br>
+                    <p class="h2">
+                        Social Media
+                    </p>
+                    <p class="fs-5">
+                        Ach jaaa: ich habe kein Snapchat und Instagram mehr, bzw. keine Social-Media-Accounts. Wie
+                        lebt es sich damit? 
+                        Ich <i>lese</i> mal einen Notizeintrag meinerseits vor:
+                        <blockquote class="blockquote">
+                            <p class="mb-0 blockquote fs-2">
+                                "Heute ist der 29. Oktober 2024. Ich habe seit 3 Tagen kein Instagram und seit heute kein 
+                                Snapchat mehr.
+                                <br>
+                                Alles fühlt sich so langsam an... so.. so menschlich...
+                                <br>
+                                Okay nein das ist nicht der Fall. Ich wünschte aber ich könnte das sagen.
+                                Leider ist alles sehr unspektakulär. Ich merke bisher nicht wirklich, dass
+                                ich keine digitalen Kontakte mehr habe und nicht im passiven Austausch zu
+                                anderen stehe"
+                            </p>
+                            <p class="blockquote-footer mt-1 text-secondary fs-5">Autor von weristmatthes.de, Matthes</p>
+                        </blockquote>
+                    </p>
+
+                    <p class="fs-5 mt-4 mb-0">
+                        Und das ist richtig! Die Notiz ist schon super duper lange her.
+                    </p>
+                    <p class="fs-5 mb-0">Wie lebt es sich denn jetzt, ca. 4 Monate später, am 19.02.2025 ohne Social-Media?</p>
+                    <p class="fs-5">Nachdem ich zumindest eine etwas längere Zeit an Erfahrung sammeln konnte, kann ich nur
+                        sagen: mach es nach. Es ist wirklich 1000x angenehmer
+                    </p>
+                    <p class="fs-5">Mein Problem waren vor Allem die ganzen Nachrichten. Die Nachrichten, die einem sagen,
+                        dass alle 2 Minuten eine Tragödie stattgefunden hat und alles schlimm ist. Das Gefühl, dass
+                        jeder Mensch um dich rum ein unglaublich tolles Leben hat und du einfach <i>ALLES</i> verpasst.
+                        Das ist doch einfach kein schönes Gefühl. Das hat mich auch immer mehr kaputt gehen lassen. Und es
+                        hört sich vermutlich übertrieben an die Accounts direkt zu löschen, aber es hat es zumindest leicht
+                        gemacht die Apps nicht wieder zu installieren. Also diese Methode bekommt meinen Daumen hoch 
+                    </p>
+                    <hr class="mt-4 mb-4" style="border-width: 0.5vh; width: 90%; margin:auto;">
+                    <p class="fs-5">Und das war es offiziell mit diesen ganzen, schon fast zufällig zusammen geordneten 
+                        Buchstaben! Danke fürs lesen!</p>
+                </div>
+            </div>
+        </div>
         
         <?php include '../html/footer.php'; ?>
     </div>
