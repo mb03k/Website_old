8ebda6ae864833f586e01d63b5f40125f83c2031
matthes <matthesboettcher03@gmail.com>
2025-02-22 21:16:00 +0100

    Eventlistener fuer homescreen hinzugefuegt

--- Script/ButtonListeners/home.js
+++ Script/ButtonListeners/home.js
@@ -5,3 +5,7 @@ document.getElementById("bootstrap_button").onclick = function() {
 document.getElementById("tailwind_button").onclick = function() {
     window.open("https://tailwindcss.com/");
 }
+
+document.getElementById("movievolunteer").onclick = function() {
+    window.open("https://weristmatthes.de/filmvorschlag/");
+}
