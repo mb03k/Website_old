4638ae8009fb2d9b14350762f041d6ea59b1518e
matthes <matthesboettcher03@gmail.com>
2025-02-22 21:13:06 +0100

    Namen der 404-Error-Page geaendert

---
+++ woistmatthes/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include '../html/head.php' ?>
+    <title>Nichts gefunden</title>
+</head>
+
+<body class="gradient">
+    <div>
+        <?php include '../html/header.php'; ?>
+        
+        <div class="container bg-info text-center w-75" style="margin-top: 10vh; margin-bottom: 500px; border-radius: 5em;">
+            <div class="row">
+                <span class="text-white" style='line-height: 90%; font-size: 25vw; font-weight: 800; font-family: "Inter", sans-serif;'>404</span>
+                <h2>nicht gefunden<h2>
+            </div>
+
+            <div>
+                <button type="button">
+                    Startseite
+                </button>
+            </div>
+        </div>
+
+        <?php include '../html/footer.php'; ?>
+    </div>
+
+    <script type="application/x-javascript" src="../Script/ButtonListeners/universalButtons.js"></script>
+    <script type="application/x-javascript" src="../Script/ReadSpeed/readSpeed.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
