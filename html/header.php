10c4b6a0380a99ec52014647ad0ac21b1217ace1
matthes <matthesboettcher03@gmail.com>
2025-02-24 14:56:37 +0100

    Neues Header-Design implementiert

--- html/header.php
+++ html/header.php
@@ -1,4 +1,4 @@
-<?php echo('
+<?php /*echo('
 <header class="bg-dark">
     <div class="header-container">
         <div class="logo home_link">
@@ -20,5 +20,23 @@
             </a>
         </nav>
     </div>
-</header>'
-);
+</header>'*/
+
+echo('
+<header class="bg-dark py-1 shadow mt-2">
+    <div class="container">
+        <div class="d-flex justify-content-between align-items-center position-relative">
+            <i class="bi bi-house text-light h1 home_link symbol_link mt-2" id="headerHomeButton"></i>
+
+            <div class="position-absolute start-50 translate-middle-x">
+                <button class="btn btn-primary portfolio_link">Portfolio</button>
+            </div>
+
+            <div class="d-flex align-items-center gap-2">
+                <img width="48" height="48" class="chess_link symbol_link" src="https://img.icons8.com/color/144/chess-com.png" alt="chess-com"/>
+                <i class="bi bi-github text-light h1 github_link symbol_link mt-2" id="headerGithubButton"></i>
+            </div>
+        </div>
+    </div>
+</header>
+');
