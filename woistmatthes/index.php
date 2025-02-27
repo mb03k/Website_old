6322e56d404bfcf6acba22cd543acfa63836981f
matthes <matthesboettcher03@gmail.com>
2025-02-27 22:09:57 +0100

    style angepasst und PHP-linkdatei eingefuegt

--- woistmatthes/index.php
+++ woistmatthes/index.php
@@ -1,7 +1,11 @@
+<?php
+include '../sicherheit/links.php';
+?>
+
 <!DOCTYPE html>
 <html lang="de">
 <head>
-    <?php include '../html/head.php' ?>
+    <?php include head ?>
     <title>Nichts gefunden</title>
     <meta name="description" content="Vermutlich hast du dich verlaufen!">
     <meta name="keywords" content="404, nicht gefunden">
@@ -9,24 +13,25 @@
 
 <body class="gradient">
     <div>
-        <?php include '../html/header.php'; ?>
+        <?php include header; ?>
         
-        <div class="container bg-info text-center w-75" style="margin-top: 10vh; margin-bottom: 500px; border-radius: 5em;">
+        <div class="container bg-info text-center w-75 p-4" style="margin-top: 10vh; margin-bottom: 300px; border-radius: 5em;">
             <div class="row">
                 <span class="text-white" style='line-height: 90%; font-size: 25vw; font-weight: 800; font-family: "Inter", sans-serif;'>404</span>
                 <h2>nicht gefunden<h2>
             </div>
 
             <div>
-                <button type="button">
+                <button class="btn btn-primary home_btn" type="button">
                     Startseite
                 </button>
             </div>
         </div>
 
-        <?php include '../html/footer.php'; ?>
+        <?php include footer; ?>
     </div>
 
     <script type="application/x-javascript" src="../Script/ButtonListeners/headerfooter.js?v2"></script>
+    <script type="application/x-javascript" src="../Script/ButtonListeners/woistmatthes.js"></script>
 </body>
 </html>
