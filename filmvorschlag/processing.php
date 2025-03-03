008980bc51be66372f61e0c7be8fb6f7ecf2f39e
matthes <matthesboettcher03@gmail.com>
2025-03-03 19:20:51 +0100

    Es werden Standort und Land von den Nutzern gespeichert

--- filmvorschlag/processing.php
+++ filmvorschlag/processing.php
@@ -2,7 +2,8 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-if (filesize('../Assets/json/filmvorschlag.json') > 5*1024*1024) { // 5 MB
+// Datei hat Limit für Größe erreicht
+if (filesize('/Applications/XAMPP/filmvorschlag.json') > 5*1024*1024) { // 5 MB
     header("Location: ./#error");
     die("Datei ist zu groß");
 }
@@ -13,41 +14,53 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     $title = htmlspecialchars($mTitle, ENT_QUOTES, 'UTF-8');
     $description = htmlspecialchars($mDescription, ENT_QUOTES, 'UTF-8');
-
+    $sent_date = date("d.m.Y");
+    $sent_time = date("H:i:s");
+    
     echo "Titel: ".$title."<br>beschreibung: ".$description."<br>";
 
     if (empty($title)) {
-        header("Location: ./#error");
+        //header("Location: ./#error");
         die("Titel eingeben");
     }
     if (strlen($title) > 100 || strlen($description) > 500) {
-        header("Location: ./#error");
+        //header("Location: ./#error");
         die("Eingaben zu groß");
     }
 
     // JSON aktualisieren:
-        $jsonFile = "../Assets/json/filmvorschlag.json";
+    $jsonFile = "/Applications/XAMPP/filmvorschlag.json";
 
-        $newJsonInput = [
-            "titel" => $title,
-            "anmerkung" => $description
-        ];
+    $user_ip = getenv('REMOTE_ADDR');
+    $geo = unserialize(file_get_contents("http://www.geoplugin.net/php.gp?ip=$user_ip"));
+    $country = $geo["geoplugin_countryName"];
+    $city = $geo["geoplugin_city"];
 
-        // Bestehende JSON-Datei einlesen
-        if (file_exists($jsonFile)) {
-            $jsonInhalt = file_get_contents($jsonFile);
-            $datenArray = json_decode($jsonInhalt, true); // JSON in Array umwandeln
-        } else {
-            echo "Fehler beim öffnen der JSON Datei";
-            header("Location: ./#error");
-            die();
-        }
+    $newJsonInput = [
+        "t" => $title,
+        "a" => $description,
+        "d" => $sent_date,
+        "z" => $sent_time,
+        "IP" => $user_ip,
+        "c" => $country,
+        "s" => $city
+    ];
 
-        array_push($datenArray, $newJsonInput);
-        file_put_contents($jsonFile, json_encode($datenArray, JSON_PRETTY_PRINT));
+    // Bestehende JSON-Datei einlesen
+    if (file_exists($jsonFile)) {
+        $jsonInhalt = file_get_contents($jsonFile);
+        $datenArray = json_decode($jsonInhalt, true); // JSON in Array umwandeln
+    } else {
+        echo "Fehler beim öffnen der JSON Datei";
+        //header("Location: ./#error");
+        die();
+    }
 
-        echo "Daten erfolgreich zu JSON angefügt";
-        header("Location: ./#success");
+    array_push($datenArray, $newJsonInput);
+    file_put_contents($jsonFile, json_encode($datenArray, JSON_PRETTY_PRINT));
+
+    echo "Daten erfolgreich zu JSON angefügt";
+    header("Location: ./#success");
 } else {
     echo "Wieso bist du hier?";
     header("Location: ../bittenicht/");
