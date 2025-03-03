88bbfe7d48dffb0b22d43788d18556a85bcaf81b
matthes <matthesboettcher03@gmail.com>
2025-03-03 19:22:41 +0100

    Links hinzugefuegt

--- sicherheit/links.php
+++ sicherheit/links.php
@@ -3,31 +3,30 @@
 error_reporting(E_ALL);
 
 if ($_SERVER['SERVER_NAME'] === 'localhost') {
-    $url = "/Applications/XAMPP/htdocs/";
+    $url = "/Applications/XAMPP/";
 } else {
-    $url = "/var/www/";
+    $url = "/var/www/html";
 }
 
+define("head", $url."html/html/head.php");
+define("header", $url."html/html/header.php");
+define("footer", $url."html/html/footer.php");
 
-define("head", $url."html/head.php");
-define("header", $url."html/header.php");
-define("footer", $url."html/footer.php");
+define("portfolio", $url."html/portfolio/");
+define("bewerbung", $url."html/portfolio/bewerbung/");
 
-define("portfolio", $url."portfolio/");
-define("bewerbung", $url."portfolio/bewerbung/");
-
-define("sessionTimeoutCheck", $url."sicherheit/sessionTimeoutCheck.php");
+define("sessionTimeoutCheck", $url."html/sicherheit/sessionTimeoutCheck.php");
 define("bewerbungstext", $url."bewerbungstext.php");
 
 define("nichthochladen", $url."nichthochladen.php");
 
-define("p_bt_audio", $url."projekte/p_bt_audio.php");
-define("p_bUhr", $url."projekte/p_bUhr.php");
-define("p_dame", $url."projekte/p_dame.php");
-define("p_drone", $url."projekte/p_drone.php");
-define("p_multimengen", $url."projekte/p_multimengen.php");
-define("p_schach", $url."projekte/p_schach.php");
-define("p_website", $url."projekte/p_website.php");
-define("p_wuerfel", $url."projekte/p_wuerfel.php");
+define("p_bt_audio", $url."html/projekte/p_bt_audio.php");
+define("p_bUhr", $url."html/projekte/p_bUhr.php");
+define("p_dame", $url."html/projekte/p_dame.php");
+define("p_drone", $url."html/projekte/p_drone.php");
+define("p_multimengen", $url."html/projekte/p_multimengen.php");
+define("p_schach", $url."html/projekte/p_schach.php");
+define("p_website", $url."html/projekte/p_website.php");
+define("p_wuerfel", $url."html/projekte/p_wuerfel.php");
 
 ?>
