5002c3dbf412eb45baf0ffd462aa3d4763d38db2
matthes <matthesboettcher03@gmail.com>
2025-03-04 16:30:31 +0100

    Funktionen zum entfernen der alten Ergebnisse eingebaut und Logik zum aufrufen der Algorithmen eingebaut

--- Script/Algorithm/saveInput.js
+++ Script/Algorithm/saveInput.js
@@ -1,58 +1,80 @@
 
+import { startBubblen, getSolution} from "./BubbleSort.js";
+import { zeichneAlgErgebnis, zeichneErsteInformationen, zeichneEingaben } from "./zeichnen.js";
+window.saveInput = saveInput; // Funktion für onclick() erreichbar machen
+window.randomise = randomise; 
 
+/* saveInput.js */
 let arrVal = [];
+let arr_length = 0;
 
-function saveInput() {
-    removeOldSolutions();
+export function saveInput() {
     for (let i=0; i<10; i++) {
         let inputFieldNum = Number(document.getElementById('inp_'+i).value);
+
+        // keine Zahl eingegeben
         if (!inputFieldNum) {
             continue;
         }
         arrVal.push(inputFieldNum);
     }
 
-    if (arrVal.length == 0) {
+    arr_length = arrVal.length;
+
+    if (!arr_length) { // nichts eingegeben
         console.log("Du bist aber lustig!");
     } else {
-        writeArrayValuesInDOM();
-        startBubblen(arrVal); // Funktion in BubbleSort.js
+        removeOldSolutions();
+        zeichneErsteInformationen(arrVal, arr_length);
+        zeichneEingaben(arrVal);
+        startBubblen(arrVal, arr_length); // Funktion in BubbleSort.js
+        let arr=getSolution();
+        zeichneAlgErgebnis(arr, arr_length);
     }
-    console.log("Mein Array vor []: "+arrVal);
+
     arrVal = [];
-    console.log("Mein Array NACH []: "+arrVal);
 }
 
-function writeArrayValuesInDOM() {
-    let arrLength = arrVal.length;
-    // n/10 Feldern beschreiben
-    for (let i=0; i<arrLength; i++) {
-        const arrPlaceholder = document.getElementById("array-td-"+i);
 
-        if (arrPlaceholder) {
-            arrPlaceholder.innerHTML = arrVal[i];
-        } // Bug: eingegebenes Array wird nicht aktualisiert wenn man neue Werte eingibt
-    }
-    // restliche Felder entfernen
-    for (let i=arrLength; i<10; i++) {
-        const notUsedTableCell = document.getElementById('array-td-'+i);
-        const notUsedTableLabel = document.getElementById('array-tl-'+i);
-
-        if (notUsedTableCell) {
-            console.log("cell ist da");
-            notUsedTableCell.remove();
-        }
-        if (notUsedTableLabel) {
-            console.log("label ist da");
-            notUsedTableLabel.remove();
+export function randomise() {
+    for (let i=0; i<10; i++) {
+        let randNum = Math.floor(Math.random() * 10); // Zahl von 0-10
+        let randPrintCheck = Math.floor(Math.random() * 10);
+        const el = document.getElementById("inp_"+i);
+        if (randPrintCheck % 2 == 0 || randPrintCheck < 5) {
+            el.value = randNum;
+        } else {
+            el.value = "";
         }
     }
 }
 
 function removeOldSolutions() {
-    alg_schritte
-    const e  = document.getElementById('alg_schritte');
-    if (e) {
-        e.innerHTML = "";
+    resestMainArr();
+    removeSteps("oldSolution");
+    removeSolution();
+}
+
+function resestMainArr() {
+    for (let i=0; i<10; i++) {
+        const el = document.getElementById("array-td-"+i);
+        el.innerHTML = "";
+        el.style.backgroundColor = "white";
+    }
+}
+
+function removeSteps(className) {
+    const elements = document.getElementsByClassName(className);
+
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
+
+function removeSolution() {
+    let table = document.getElementById("input_solution");
+
+    while (table.firstChild) {
+        table.removeChild(table.firstChild);
     }
 }
